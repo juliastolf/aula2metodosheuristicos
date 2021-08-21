8e062164fe30a8cc5ac880b503e03b066620caac
juliastolf <89302772+juliastolf@users.noreply.github.com>
2021-08-21 11:00:43 -0300

    Aula2

---
+++ Scripts.R
@@ -0,0 +1,25 @@
+## Configurando diretório
+
+getwd()
+
+## Contribuidores
+contributors()
+
+## Intalar pacotes
+intalled.packages()
+intall.packages("randomForest")
+
+## Carregando pacotes
+library(randomForest)
+
+## Descarregando Pacotes
+detach(package:randomforest)
+
+##Ajuda
+help("detach")
+
+install.packages("sos")
+
+library(sos)
+
+findFn("detach")
